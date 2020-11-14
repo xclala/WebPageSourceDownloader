6f14bca019fec8feaef2031cd735deec0bd79b49
xclala <3035699035@qq.com>
2020-11-14 15:49:37 +0800

    首次代码提交

---
+++ main.py
@@ -0,0 +1,18 @@
+try:
+    from os import system
+    url = input("url:")
+    savedPath = input("saved path:")
+    if url == "":
+        print("请输入url!")
+    else:
+        if savedPath == "":
+            from os import getcwd
+            path = getcwd()
+            system(
+                f'wget -r --no-parent --reject "index.html*" {url} -P {path}')
+        else:
+            system(
+                f'wget -r --no-parent --reject "index.html*" {url} -P {savedPath}')
+    system("pause")
+except Exception as e:
+    print(e)
