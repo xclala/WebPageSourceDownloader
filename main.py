1619d707d03efe2760e9c2f04850961dc1d7928f
xclala <3035037699@qq.com>
2020-11-14 17:58:24 +0800

    增加简单的反爬虫

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ try:
             from os import getcwd
             path = getcwd()
             system(
-                f'wget -c -k -r -L -p --no-parent --reject "index.html*" {url} -P {path}')
+                f'wget --user-agent="User-Agent: Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.193 Mobile Safari/537.36" -c -k -r -L -p --no-parent --reject "index.html*" {url} -P {path}')
         else:
             system(
-                f'wget -c -k -r -L -p --no-parent --reject "index.html*" {url} -P {savedPath}')
+                f'wget --user-agent="User-Agent: Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.193 Mobile Safari/537.36" -c -k -r -L -p --no-parent --reject "index.html*" {url} -P {savedPath}')
     system("pause")
 except Exception as e:
     print(e)
